6eb65e0ea993b3cf95138ee2ccecc176453f855a
Sneha Dubey <dubeysneha327@gmail.com>
2026-03-06 23:12:09 +0530

    fix services path new 1

--- api/upload.js
+++ api/upload.js
@@ -1,5 +1,5 @@
 const Busboy = require("busboy")
-const { compressPDF } = require("../services/pdfCompressor")
+const { compressPDF } = require("../services/pdf_compression.js")
 // const { scanFile } = require("../services/fileSecurity")
 const { uploadToGithub } = require("../services/githubService")
 const { scanFile } = require("../services/fileScanner")
